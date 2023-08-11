7432774cca6b2f975b45e5d4164a31ed54232dbf
rshezarr <rshezarr@gmail.com>
2023-08-11 11:09:17 +0600

    add: config struct and parse configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,29 @@
 package config
 
+import (
+	"github.com/spf13/viper"
+	"log"
+	"time"
+)
+
 type Config struct {
+	API API `yaml:"api"`
 }
 
-func NewConfig(cfgFilePath string) *Config {
-	return &Config{}
+type API struct {
+	Port           string        `yaml:"port"`
+	MaxHeaderBytes int           `yaml:"maxHeaderBytes"`
+	Timeout        time.Duration `yaml:"timeout"`
+}
+
+func NewConfig() *Config {
+	var cfg *Config
+
+	//unmarshal config into "config" struct
+	if err := viper.Unmarshal(&cfg); err != nil {
+		log.Fatalf("error while parsing configs: %v", err)
+		// return nil
+	}
+
+	return cfg
 }
