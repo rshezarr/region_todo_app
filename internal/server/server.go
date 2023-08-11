1d32c4ee5a795e4de34b7d745c09fc067f6cb46f
rshezarr <rshezarr@gmail.com>
2023-08-11 11:10:49 +0600

    edit: server: add config struct and add graceful shutdown

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,22 +3,38 @@ package server
 import (
 	"context"
 	"net/http"
-	"time"
 	"todo_list/internal/config"
 )
 
 type Server struct {
-	srv *http.Server
+	srv               *http.Server
+	ServerErrorNotify chan error
 }
 
-func NewServer(ctx *context.Context, cfg *config.Config) *Server {
+func NewServer(ctx context.Context, cfg *config.Config) *Server {
 	return &Server{
 		srv: &http.Server{
-			Addr:           ":9090",
+			Addr:           cfg.API.Port,
 			Handler:        nil,
-			MaxHeaderBytes: 1 << 20,
-			WriteTimeout:   time.Duration(5 * time.Second),
-			ReadTimeout:    time.Duration(5 * time.Second),
+			MaxHeaderBytes: cfg.API.MaxHeaderBytes << 20,
+			WriteTimeout:   cfg.API.Timeout,
+			ReadTimeout:    cfg.API.Timeout,
 		},
+		ServerErrorNotify: make(chan error, 1),
 	}
+}
+
+// run server while sending errors to error channel
+func (s *Server) Start() {
+	s.ServerErrorNotify <- s.srv.ListenAndServe()
+}
+
+// accept errors into channel
+func (s *Server) ServerErrNotify() <-chan error {
+	return s.ServerErrorNotify
+}
+
+// shutdown function
+func (s *Server) Shutdown() error {
+	return s.srv.Shutdown(context.Background())
 }
