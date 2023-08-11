32a15dfa76df0a1692e90ee1701fa2c80d621c16
rshezarr <rshezarr@gmail.com>
2023-08-11 11:11:26 +0600

    edit: app package: build project and graceful shutdown

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,46 @@
 package app
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"todo_list/internal/config"
+	"todo_list/internal/server"
+)
+
+func Run() {
+	cfg := config.NewConfig()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	srv := server.NewServer(context.Background(), cfg)
+
+	// graceful shutdown
+	// creating channel for signals
+	quit := make(chan os.Signal, 1)
+
+	// accepting signal from user
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+
+	// run server in goroutine
+	go func() {
+		log.Printf("app: Starting server at port %v -> http:// localhost%v\n", cfg.API.Port, cfg.API.Port)
+		srv.Start()
+	}()
+
+	// "select" makes the server wait for any signal or error
+	select {
+	case sig := <-quit:
+		log.Printf("app: signal accepted: %v\n", sig)
+	case err := <-srv.ServerErrNotify():
+		log.Printf("app: server closing: %v\n", err)
+	}
+
+	// shutting down the server when any error or signal occurs
+	if err := srv.Shutdown(); err != nil {
+		log.Printf("error while shutting down server: %s\n", err.Error())
+	}
+}
