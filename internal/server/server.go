d87fbd1b9e7a822eb96f9aec1276e1b7624c2e45
rshezarr <rshezarr@gmail.com>
2023-08-11 10:28:55 +0600

    add: server struct and constructor

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,24 @@
 package server
+
+import (
+	"context"
+	"net/http"
+	"time"
+	"todo_list/internal/config"
+)
+
+type Server struct {
+	srv *http.Server
+}
+
+func NewServer(ctx *context.Context, cfg *config.Config) *Server {
+	return &Server{
+		srv: &http.Server{
+			Addr:           ":9090",
+			Handler:        nil,
+			MaxHeaderBytes: 1 << 20,
+			WriteTimeout:   time.Duration(5 * time.Second),
+			ReadTimeout:    time.Duration(5 * time.Second),
+		},
+	}
+}
