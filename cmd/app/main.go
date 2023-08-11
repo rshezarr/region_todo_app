65a3130cf174ee21c7f4c197de42e50b1ab87a86
rshezarr <rshezarr@gmail.com>
2023-08-11 11:12:05 +0600

    add: config initialization and app run func

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/spf13/viper"
+	"log"
+	"todo_list/internal/app"
+)
 
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error occured while parsing configs: %v", err)
+	}
+
+	app.Run()
+}
+
+func initConfig() error {
+	//set flag
+	var configPath = flag.String("config-path", "configs/", "path to config file")
+
+	//parse flag
+	flag.Parse()
+
+	//set config file path, name and type
+	viper.SetConfigName("configs")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(*configPath)
+
+	//read config by property above
+	return viper.ReadInConfig()
 }
