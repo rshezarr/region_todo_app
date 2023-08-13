83ad1fb49d686affc5c99d03e6277171af18df47
rshezarr <rshezarr@gmail.com>
2023-08-13 16:39:08 +0600

    add: connect mongo function

--- pkg/database/mongodb/mongodb.go
+++ pkg/database/mongodb/mongodb.go
@@ -1,1 +1,22 @@
 package mongodb
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB(ctx context.Context) (*mongo.Database, error) {
+	cliOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	client, err := mongo.Connect(ctx, cliOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	return client.Database("todo_list_db"), nil
+}
