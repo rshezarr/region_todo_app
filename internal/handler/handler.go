fcf06c0e131163440f5bf2ad22ac9946dfd0af0b
rshezarr <rshezarr@gmail.com>
2023-08-13 16:46:59 +0600

    add: handler struct and constructor

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,13 @@
 package handler
+
+import "todo_list/internal/service"
+
+type Handler struct {
+	svc *service.Service
+}
+
+func NewHandler(svc *service.Service) *Handler {
+	return &Handler{
+		svc: svc,
+	}
+}
