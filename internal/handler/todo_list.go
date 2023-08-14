ae58c6ee729b0e82215ab8c106088795984771d7
rshezarr <rshezarr@gmail.com>
2023-08-14 15:57:15 +0600

    add: create list handler

--- internal/handler/todo_list.go
+++ internal/handler/todo_list.go
@@ -1,9 +1,28 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"todo_list/internal/model"
+)
 
 func (h *Handler) CreateListHandler(c *gin.Context) {
+	list := model.List{}
 
+	if err := c.BindJSON(&list); err != nil {
+		c.Error(err)
+		return
+	}
+
+	id, err := h.svc.TodoList.CreateList(c.Copy(), list)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"list_id": id,
+	})
 }
 
 func (h *Handler) GetListsHandler(c *gin.Context) {
