a471b66bd3d064e5c12c6836dc8d29e6a6fac013
rshezarr <rshezarr@gmail.com>
2023-08-14 16:11:19 +0600

    add: get list handler

--- internal/handler/todo_list.go
+++ internal/handler/todo_list.go
@@ -20,13 +20,23 @@ func (h *Handler) CreateListHandler(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, map[string]interface{}{
+	c.JSON(http.StatusNoContent, map[string]interface{}{
 		"list_id": id,
 	})
 }
 
 func (h *Handler) GetListsHandler(c *gin.Context) {
+	status := c.Query("status")
 
+	lists, err := h.svc.TodoList.GetList(c.Copy(), status)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"lists": lists,
+	})
 }
 
 func (h *Handler) UpdateListHandler(c *gin.Context) {
