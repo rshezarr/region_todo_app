1490d20026b78ee9dbd92f0ad7d704b6ae925197
rshezarr <rshezarr@gmail.com>
2023-08-14 15:50:34 +0600

    add: todo list handlers

--- internal/handler/todo_list.go
+++ internal/handler/todo_list.go
@@ -1,1 +1,19 @@
 package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) CreateListHandler(c *gin.Context) {
+
+}
+
+func (h *Handler) GetListsHandler(c *gin.Context) {
+
+}
+
+func (h *Handler) UpdateListHandler(c *gin.Context) {
+
+}
+
+func (h *Handler) DeleteListHandler(c *gin.Context) {
+
+}
