5cdc013492eac372bb5de400367af34589d767d1
rshezarr <rshezarr@gmail.com>
2023-08-14 15:23:27 +0600

    init: todo list service

--- internal/service/service.go
+++ internal/service/service.go
@@ -5,11 +5,8 @@ import (
 )
 
 type Service struct {
-	repo *repository.Repository
 }
 
 func NewService(repo *repository.Repository) *Service {
-	return &Service{
-		repo: repo,
-	}
+	return &Service{}
 }
