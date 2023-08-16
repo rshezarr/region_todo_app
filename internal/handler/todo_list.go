cfd5cdf589768e4bfd78abb21a3c508614248d2c
rshezarr <rshezarr@gmail.com>
2023-08-16 21:54:33 +0600

    fix: handler responses

--- internal/handler/todo_list.go
+++ internal/handler/todo_list.go
@@ -2,27 +2,31 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	"todo_list/internal/model"
 )
 
 func (h *Handler) CreateListHandler(c *gin.Context) {
-	list := model.List{}
+	var list model.List
 
 	if err := c.BindJSON(&list); err != nil {
 		c.Error(err)
+		log.Printf("error while bind json: %v", err)
 		return
 	}
 
 	id, err := h.svc.TodoList.CreateList(c.Copy(), list)
 	if err != nil {
 		c.Error(err)
+		log.Printf("error while creating list: %v", err)
 		return
 	}
 
-	c.JSON(http.StatusNoContent, map[string]interface{}{
-		"list_id": id,
-	})
+	_, err = c.Writer.WriteString(id)
+	if err != nil {
+		c.Error(err)
+	}
 }
 
 func (h *Handler) GetListsHandler(c *gin.Context) {
@@ -40,9 +44,35 @@ func (h *Handler) GetListsHandler(c *gin.Context) {
 }
 
 func (h *Handler) UpdateListHandler(c *gin.Context) {
+	id := c.GetString("id")
 
+	newList := model.List{
+		ID: id,
+	}
+
+	if err := c.BindJSON(&newList); err != nil {
+		c.Error(err)
+		return
+	}
+
+	id, err := h.svc.TodoList.UpdateList(c.Copy(), newList)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusNoContent, map[string]interface{}{
+		"updated_list_id": id,
+	})
 }
 
 func (h *Handler) DeleteListHandler(c *gin.Context) {
+	id := c.GetString("id")
 
+	if err := h.svc.TodoList.DeleteList(c.Copy(), id); err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusNoContent, nil)
 }
