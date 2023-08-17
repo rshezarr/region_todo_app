de03ee3ec7347ae94deaf10f6985d8cd50b9f0b7
rshezarr <rshezarr@gmail.com>
2023-08-17 12:25:36 +0600

    add: index for unique combination

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,12 +1,31 @@
 package repository
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
 
 type Repository struct {
 	TodoList TodoList
 }
 
 func NewRepository(db *mongo.Database) *Repository {
+	db.CreateCollection(context.Background(), "lists")
+	collection := db.Collection("lists")
+	indexModel := mongo.IndexModel{
+		Keys:    bson.D{{Key: "title", Value: 1}, {Key: "activeAt", Value: 1}},
+		Options: options.Index().SetUnique(true).SetSparse(true),
+	}
+
+	// Create the unique compound index
+	_, err := collection.Indexes().CreateOne(context.Background(), indexModel)
+	if err != nil {
+		log.Fatalf("error while setting index: %v\n", err)
+	}
+
 	return &Repository{
 		TodoList: NewTodoListRepo(db),
 	}
